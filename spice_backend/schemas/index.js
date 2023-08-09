7412f2b1e7a158979c9d5bca195c2dfa7d1bdf84
peppersoupspice <michaeliyinboh@gmail.com>
2023-08-09 19:21:21 +0100

    finished backend with sanity io

--- spice_backend/schemas/index.js
+++ spice_backend/schemas/index.js
@@ -1,3 +1,9 @@
-import user from './user'
+import user from './user';
+import pin from './pin';
+import comment from './comment';
+import postedBy from './postedBy';
+import save from './save';
 
-export const schemaTypes = [user]
+export const schemaTypes = [
+    user, pin, comment, postedBy, save
+]
