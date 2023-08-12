c4762e44d1c7c383f42d0736a2c9f25c943d7ff9
peppersoupspice <michaeliyinboh@gmail.com>
2023-08-12 17:16:55 +0100

    set up frontend with react, tailwindd, route packages

---
+++ spice_frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Routes, Route, useNavigate} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
